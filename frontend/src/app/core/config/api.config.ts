50eea31327902e0cad05bb0ef6ced423873f2ec8
Prince1337 <prince.pieritz@gmail.com>
2025-07-10 12:04:07 +0200

    fix: link and gitignore

--- frontend/src/app/core/config/api.config.ts
+++ frontend/src/app/core/config/api.config.ts
@@ -3,5 +3,5 @@ export class ApiConfig {
    * Basis-URL für alle API-Endpunkte
    * Ändern Sie diese URL hier, um sie für alle Service-Klassen zu aktualisieren
    */
-  public static readonly API_BASE_URL = 'https://crm-c0bd6-api.de/api';
+  public static readonly API_BASE_URL = 'http://localhost:8080/api';
 } 
